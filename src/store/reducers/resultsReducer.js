2197e3ba0feecddd5b2228f65cddb933fbb1708d
Yang Jae Hoon <didwongs789@naver.com>
2020-09-24 17:32:15 +0900

    A Leaner Switch Case Statement

--- src/store/reducers/resultsReducer.js
+++ src/store/reducers/resultsReducer.js
@@ -5,23 +5,28 @@ const initialState = {
   results: [],
 };
 
+const storeResult = (state, action) => {
+  const updatedResults = state.results.filter(
+    (result) => result.id !== action.resultEleId
+  );
+  return updatedObject(state, { results: updatedResults });
+};
+
+const deleteResult = (state, action) => {
+  const updatedResults = state.results.concat({
+    id: new Date(),
+    value: action.result,
+  });
+  return updatedObject(state, { results: updatedResults });
+};
+
 const reducer = (state = initialState, action) => {
   // eslint-disable-next-line
   switch (action.type) {
-    case actionTypes.STORE_RESULT: {
-      const updatedResults = state.results.concat({
-        id: new Date(),
-        value: action.result,
-      });
-      return updatedObject(state, { results: updatedResults });
-    }
-
-    case actionTypes.DELETE_RESULT: {
-      const updatedResults = state.results.filter(
-        (result) => result.id !== action.resultEleId
-      );
-      return updatedObject(state, { results: updatedResults });
-    }
+    case actionTypes.STORE_RESULT:
+      return deleteResult(state, action);
+    case actionTypes.DELETE_RESULT:
+      return storeResult(state, action);
   }
 
   return state;
